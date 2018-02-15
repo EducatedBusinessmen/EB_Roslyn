141e27bc381929fc35204f14c2d49c992b052006
Roslyn <dimnaut@gmail.com>
2018-02-14 18:05:55 -0800

    Tank Drive

--- src/org/usfirst/frc/team7096/robot/Robot.java
+++ src/org/usfirst/frc/team7096/robot/Robot.java
@@ -7,79 +7,153 @@
 
 package org.usfirst.frc.team7096.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Timer;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.CameraServer;
+import java.lang.Math.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-	private DifferentialDrive m_robotDrive
+	DifferentialDrive robotDrive
 			= new DifferentialDrive(new Spark(0), new Spark(1));
-	private Joystick m_stick = new Joystick(0);
-	private Timer m_timer = new Timer();
-	private String color = "unassigned";
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	
+	Joystick operator = new Joystick(0);
+	Joystick joystick_L = new Joystick(1);
+	Joystick joystick_R = new Joystick(2);
+	Joystick gamepad = new Joystick(3);
+	
+	Servo servo = new Servo(2);
+	TalonSRX panTalon = new TalonSRX(0);
+	Encoder enc = new Encoder(0,1);
+	Timer timer = new Timer();
+	
+	double vMult = 1.0;
+	double a = 0;
+	
+	boolean pov = false;
+	
+	boolean previousButton = false;
+	boolean currentButton = false;
+	boolean vHalf = false;
+	
 	@Override
 	public void robotInit() {
 		CameraServer.getInstance().startAutomaticCapture(0);
-	    CameraServer.getInstance().startAutomaticCapture(1);
-	    System.out.println("Our color is " + color);
+		CameraServer.getInstance().startAutomaticCapture(1);
 	}
-
-	/**
-	 * This function is run once each time the robot enters autonomous mode.
-	 */
+	
 	@Override
 	public void autonomousInit() {
-		m_timer.reset();
-		m_timer.start();
+		timer.reset();
+		timer.start();
 	}
-
-	/**
-	 * This function is called periodically during autonomous.
-	 */
+	
 	@Override
 	public void autonomousPeriodic() {
-		// Drive for 2 seconds
-		if (m_timer.get() < 2.0) {
-			m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-		} else {
-			m_robotDrive.stopMotor(); // stop robot
+		
+		if (timer.get() < 1.0) {
+			robotDrive.arcadeDrive(0.4, 0);
+		}
+		else if(timer.get() < 3.0) {
+			robotDrive.arcadeDrive(0, 0.4);
+		}
+		else if(timer.get() < 5.0) {
+			robotDrive.arcadeDrive(0, -0.4);
+		}
+		else {
+			robotDrive.stopMotor();
 		}
 	}
-
-	/**
-	 * This function is called once each time the robot enters teleoperated mode.
-	 */
+		
 	@Override
 	public void teleopInit() {
+		enc.reset();
+		vMult = 1.0;
+		robotDrive.stopMotor();
+		
+		previousButton = false;
+		currentButton = false;
+		vHalf = false;
+		
+		a = 1.0; //constant (0 to 1)
 	}
 
-	/**
-	 * This function is called periodically during teleoperated mode.
-	 */
+	boolean tankDriveMode = true;
+	
+	public double joyMod(double x) {
+		double xP = 0;
+		if(Math.abs(x) < 0.01) {
+			x = (int)x;
+			x = (double)x;
+		}
+		if(x<0) {
+			x = Math.abs(x);
+			xP = a*(Math.pow(x, (1.0/3.0))) + (1-a)*x;
+			xP = -xP;
+		}
+		else {
+			xP = a*(Math.pow(x, (1.0/3.0))) + (1-a)*x;
+		}
+		return xP;
+	}
+	
 	@Override
 	public void teleopPeriodic() {
-		m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
-	}
+		
+		previousButton = currentButton;
+		currentButton = operator.getRawButton(10);
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
+		if (currentButton && !previousButton) 
+		{
+			vHalf = !vHalf;
+			
+			if(vHalf) {
+				a = 1.0;
+				System.out.println("toggle_1");
+			}
+			else {
+				a = 1.0;
+				System.out.println("toggle_2");
+			}
+		}
+		
+		//Drive Mode Set
+		if (joystick_L.getRawButton(2) || gamepad.getRawButton(2)) {
+			tankDriveMode = !tankDriveMode;
+			System.out.println("Tank Drive" + tankDriveMode);
+		}
+		//Arcade Drive
+		if (!tankDriveMode) {
+			robotDrive.arcadeDrive(joystick_L.getY(), joystick_L.getX());
+		}
+		//Tank Drive
+		else {
+			robotDrive.tankDrive(gamepad.getRawAxis(1), gamepad.getRawAxis(5));
+			
+			double R = joyMod(joystick_R.getRawAxis(1));
+			double L = joyMod(joystick_L.getRawAxis(1));
+			System.out.println(R + " " + L);
+			robotDrive.tankDrive(R, L);
+		}
+		
+		//prints
+		if (!tankDriveMode && joystick_L.getRawButton(5)) {
+			System.out.println(joystick_L.getPOV());
+		}
+		else if (tankDriveMode && gamepad.getRawButton(5)) {
+			System.out.println(gamepad.getPOV());
+		}
+		else if (!tankDriveMode && joystick_L.getRawButton(3)) {
+			System.out.println(joystick_L.getPort());
+		}
+		
+		else if (tankDriveMode && gamepad.getRawButton(3)) {
+			System.out.println(gamepad.getPort());
+		}
+	}
+	
 	@Override
 	public void testPeriodic() {
+		
 	}
 }
